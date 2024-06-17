3f2e0e80a45c057dcc6f02994ad848f7cbda3c92
Дмитрий Шундрик <dmitryshundrik@gmail.com>
2024-06-17 21:38:03 +0300

    Created simple Spring Boot application with one controller and HTML page.

---
+++ spring-boot/src/main/java/com/example/springboot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String getHomePage(Model model) {
+        model.addAttribute("text", "Welcome Home!");
+        return "home";
+    }
+
+}
